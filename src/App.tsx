c91c14921ed3a5174a7f2db2745f1986e182ac25
Richard Breslin <richardbreslin@wayne.edu>
2021-12-09 14:29:10 -0500

    Add ItemsList Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import AppHeader from './todoList/AppHeader';
+import ItemsList from './todoList/ItemsList';
 
 function App() {
   return (
     <div className="quick-list-app">
-      
-      <AppHeader /> 
-
-      <ul className="items-list">
-          <li className="todo-item">to</li>
-          <li className="todo-item">be</li>
-          <li className="todo-item">finished</li>
-      </ul>
-
-      <div className="action-section">
-          <button className="remove-btn"> remove</button>
-          <button className="add-btn">Add</button>
-      </div>
+      <AppHeader />
+      <ItemsList />
     </div>
   );
 }
--- src/todoList/ItemsList.tsx
+++ src/todoList/ItemsList.tsx
@@ -1,9 +1,93 @@
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import './ItemsList.css';
 
-require("./logo.css");
-export default function Logo() {
+type Props = {}
+type State = {
+  newItem: string,
+  list: newItemEntry[],
+}
+type newItemEntry = {
+  id: number,
+  value: string;
+}
+
+class ItemsList extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: [],
+    };
+  }
+
+  updateInput(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ newItem: event.target.value });
+  }
+
+  addItem() {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice(),
+    };
+
+    const tempList: newItemEntry[] = [...this.state.list];
+
+    tempList.push(newItem);
+
+    this.setState({
+      newItem: "",
+      list: tempList
+    });
+
+  }
+  deleteItem(id: number) {
+    const list = [...this.state.list];
+
+    const updatedList = list.filter(function (item) {
+      if (item.id !== id) {
+        return item;
+      }
+    });
+
+    this.setState({ list: updatedList });
+  }
+
+  render() {
     return (
-      <ul className="App">
-        <div className="quick-list-logo">QuickList</div>
-      </ul>
+      <div className="ItemsList">
+        <Container className="p-3">
+
+          <h1 className="header">
+            Enter Item
+          </h1>
+          <input
+            type="text"
+            placeholder="type item here..."
+            value={this.state.newItem}
+            onChange={(e) => this.updateInput(e)}
+          />
+          <button
+            onClick={() => this.addItem()}
+            disabled={!this.state.newItem.length}
+          >
+            Add
+          </button>
+          <br />
+          <ul>
+            {this.state.list.map((item) => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)}>X</button>
+                </li>
+              );
+            })}
+          </ul>
+        </Container>
+      </div>
     );
-  };
+  }
+}
+
+export default ItemsList;
