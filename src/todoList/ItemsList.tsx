47e04289bb0e1d430a52d044f102c6184d1d2b51
Richard Breslin <richardbreslin@wayne.edu>
2021-12-09 20:22:07 -0500

    Update ItemsList.tsx

--- src/todoList/ItemsList.tsx
+++ src/todoList/ItemsList.tsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup'
-import Badge from 'react-bootstrap/ListGroup'
+import Badge from 'react-bootstrap/Badge'
 import Form from 'react-bootstrap/Form'
 import './ItemsList.css';
 
@@ -15,57 +15,96 @@ import './ItemsList.css';
 //proirity level
 //status ( active or completed)
 
-type Props = {}
-type State = {
-  descInput: string,
-  taskList: TaskEntry[],
-}
+
 
 type TaskEntry = {
   id: number,
   description: string;
   date: string,
-  category?: CategoryType,
-  priority?: 1 | 2 | 3 | 4,
+  category: CategoryType,
+  priority?: number,
   status: "active" | "complete"
 }
 type CategoryType = {
   name: string,
-  parentCategory: CategoryType | null
+  parentCategory?: CategoryType | null
 }
 
+type Props = {}
+type State = {
+  taskInput: TaskEntry,
+  taskList: TaskEntry[],
+}
 class ItemsList extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      descInput: "",
+      taskInput: {
+        id: 0,
+        description: "",
+        date: "",
+        category: {
+          name: " ",
+          parentCategory: null
+        },
+        priority: 0,
+        status: "active"
+      },
       taskList: [],
     };
   }
 
-  updateInput(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ descInput: event.target.value });
+  updateDescInput(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      taskInput: { ...this.state.taskInput, description: event.target.value }
+    });
   }
+  updateCategoryInput(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      taskInput: { ...this.state.taskInput, category: { name: event.target.value } }
+    });
+  }
+  updateDate(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      taskInput: { ...this.state.taskInput, date: event.target.value }
+    });
+  }
+  updatePriority(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({
+      taskInput: { ...this.state.taskInput, priority: Number(event.target.value) }
+    });
+  }
+
 
   addItem() {
 
-    const taskDescInput: TaskEntry = {
+    const taskInput: TaskEntry = {
       id: 1 + Math.random(),
-      description: this.state.descInput.slice(),
-      date: " ",
-      category: { name: " ", parentCategory: null },
-      priority: 1,
+      description: this.state.taskInput.description.slice(),
+      date: this.state.taskInput.date,
+      category: { name: this.state.taskInput.category.name, parentCategory: null },
+      priority: this.state.taskInput.priority,
       status: "active"
     };
 
     const tempList: TaskEntry[] = [...this.state.taskList];
-
-    tempList.push(taskDescInput);
+    tempList.push(taskInput);
 
     this.setState({
-      descInput: "",
+      taskInput: {
+        id: 0,
+        description: "",
+        date: "",
+        category: {
+          name: " ",
+          parentCategory: null
+        },
+        priority: 0,
+        status: "active"
+      },
       taskList: tempList
     });
+
 
   }
   deleteItem(id: number) {
@@ -94,24 +133,34 @@ class ItemsList extends React.Component<Props, State> {
                   <Form.Label>Enter Task Description</Form.Label>
                   <Form.Control as="textarea" rows={3} type="text"
                     placeholder="type item here..."
-                    value={this.state.descInput}
-                    onChange={(e: any) => this.updateInput(e)} />
+                    value={this.state.taskInput.description}
+                    onChange={(e: any) => this.updateDescInput(e)} />
                 </Form.Group>
                 <Form.Group className="mb-3">
                   <Form.Label>Task Category</Form.Label>
-                  <Form.Control placeholder="enter category here" />
+                  <Form.Control type="text"
+                    placeholder="type category here..."
+                    value={this.state.taskInput.category?.name}
+                    onChange={(e: any) => this.updateCategoryInput(e)} />
                 </Form.Group>
                 <Row>
                   <Col>
                     <Form.Group className="mb-3">
                       <Form.Label>Select due date</Form.Label>
-                      <Form.Control type="date" name='due_date' />
+                      <Form.Control
+                        type="date"
+                        name='due_date'
+                        value={this.state.taskInput.date}
+                        onChange={(e: any) => this.updateDate(e)} />
                     </Form.Group>
                   </Col>
                   <Col>
                     <Form.Group className="mb-3" >
                       <Form.Label>Priority Level</Form.Label>
-                      <Form.Select >
+                      <Form.Select
+                        value={this.state.taskInput.priority}
+                        onChange={(e: any) => this.updatePriority(e)}>
+                        <option value="0">None</option>
                         <option value="1">1 - Highest</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -122,8 +171,7 @@ class ItemsList extends React.Component<Props, State> {
                 </Row>
                 <Button
                   onClick={() => this.addItem()}
-                  disabled={!this.state.descInput.length}
-                  type="submit"
+                  disabled={!this.state.taskInput.description.length || !this.state.taskInput.date.length}
                   variant="primary"
                 >
                   Add
@@ -132,31 +180,37 @@ class ItemsList extends React.Component<Props, State> {
               <br />
               <h1>Current Task List</h1>
               <br />
+
               <ul>
-                {this.state.taskList.map((item) => {
+                {this.state.taskList.map((item: TaskEntry) => {
                   return (
 
                     <div>
-                      <ListGroup as="ol" numbered>
+
+                      <ListGroup as="ul">
+
                         <ListGroup.Item
                           as="li"
                           className="d-flex justify-content-between align-items-start"
                         >
+                          <Button variant="danger">delete</Button>
                           <div className="ms-2 me-auto">
-                            <div className="fw-bold">{item.description}</div>
-                            something here
+                            <div className="fw-bold">{item.date}</div>
+                            {item.description.length}
                           </div>
-                          <Badge>
-                            {/* {item.category} */}
-                          </Badge>
+                          {(item.category.name.length > 1) ? <Badge bg="primary" pill> Category:{item.category.name} </Badge> : null}
+                          {(item.priority == 0) ? null : <Badge bg="secondary" pill> Priority: {item.priority} </Badge>}
                         </ListGroup.Item>
                       </ListGroup>
                     </div>
                   );
                 })}
               </ul>
+
+
             </Col>
           </Row>
+
         </Container>
       </div >
     );
